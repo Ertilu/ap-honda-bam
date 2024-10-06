8b3cc4a40f63f66501f536097a4011b058e52492
Reza Raka Nugraha <rezaraka00@gmail.com>
2024-10-06 17:15:54 +0700

    install partyscoket

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const PARTYKIT_HOST = process.env.REACT_APP_PUBLIC_PARTYKIT_HOST
+
+export const PARTYKIT_PROTOCOL =
+  PARTYKIT_HOST?.startsWith('localhost') || PARTYKIT_HOST?.startsWith('127.0.0.1')
+    ? 'http'
+    : 'https'
+
+export const PARTYKIT_URL = `${PARTYKIT_PROTOCOL}://${PARTYKIT_HOST}`
